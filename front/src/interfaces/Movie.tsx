294106b24e6db2faa9ac337fd03d1b1940a78713
Alexandre Mattje <amattje@gmail.com>
2019-09-05 12:20:29 -0300

    created moviecard

---
+++ front/src/interfaces/Movie.tsx
@@ -0,0 +1,8 @@
+export default interface Movie {
+    id: number;
+    title: String;
+    genre_ids: Array<String>;
+    poster_path: String;
+    backdrop_path: String;
+
+}
--- front/src/pages/Movies.tsx
+++ front/src/pages/Movies.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "./movies.css";
 import TextField from "@material-ui/core/TextField";
-
-interface Movie {
-    id: number;
-    title: String;
-    poster_path: String;
-    backdrop_path: String;
-}
+import "./movies.css";
+import Movie from "../interfaces/Movie";
+import MovieCard from "./components/MovieCard";
 
 export default () => {
     const [page, setPage] = useState(1);
@@ -29,15 +24,7 @@ export default () => {
         <div>
             <TextField variant="outlined" label="Search value" />
             {movies.map(movie => (
-                <div className="container">
-                    <div className="card container">
-                        <div>{movie.title}</div>
-                        <img
-                            src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
-                            alt="Poster"
-                        ></img>
-                    </div>
-                </div>
+                <MovieCard movie={movie} />
             ))}
         </div>
     );
