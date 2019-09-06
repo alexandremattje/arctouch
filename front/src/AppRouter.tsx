d6df8d8e903186c384ed7194087ae298385a04e0
Alexandre Mattje <amattje@gmail.com>
2019-09-06 08:48:28 -0300

    removing unused compo

--- front/src/AppRouter.tsx
+++ front/src/AppRouter.tsx
@@ -3,7 +3,6 @@ import {
     BrowserRouter as Router,
     Route
 } from "react-router-dom";
-import { QueryParamProvider } from 'use-query-params';
 import Movies from './pages/Movies';
 import MovieDetail from './pages/MovieDetail';
 
@@ -11,12 +10,10 @@ import MovieDetail from './pages/MovieDetail';
 export default () => {
     return (
         <Router>
-            <QueryParamProvider>
-                <div>
-                    <Route path="/" exact component={Movies} />
-                    <Route path="/movie" component={MovieDetail} />
-                </div>
-            </QueryParamProvider>
+            <div>
+                <Route path="/" exact component={Movies} />
+                <Route path="/movie" component={MovieDetail} />
+            </div>
         </Router>
     );
 };
